e45465a0eee8d503ca84c43862f074b66c28d00c
shullmb <dev.shullmb@gmail.com>
2015-07-26 22:33:50 -0700

    in process

---
+++ FCC_TruncateString.js
@@ -0,0 +1,18 @@
+//Free Code Camp Bonfire: Truncate a String
+
+/*
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a '...' ending.
+
+Input:
+Output:
+
+
+
+*/
+
+function truncate(str, num) {
+	
+}
+
+truncate('A-tisket a-tasket A green and yellow basket', 11);
