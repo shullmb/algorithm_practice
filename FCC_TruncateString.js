2151bfe3960ca3ce59bde190bfbb6a19929680a3
shullmb <dev.shullmb@gmail.com>
2015-07-27 06:00:56 -0700

    in process

--- FCC_TruncateString.js
+++ FCC_TruncateString.js
@@ -4,16 +4,27 @@
 Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
 Return the truncated string with a '...' ending.
 
-Input: String s, Integer n for max string length
+Input: String s with length n, Integer m for max string length
 Output: truncated string with ellipses tacked on end
-
-
+	
+If length n > m
+	sliceStart = 0
+	sliceEnd = m - 3
+	concatenate truncated string + ...
+Else 
+	do not slice string
 
 
 
 */
 
 function truncate(str, num) {
+	var sliced = str.slice(0, num - 3)
+	if (sliced.length > num ) {
+		return sliced + "...";
+	} else {
+		return "This isn't working yet"
+	}
 	
 }
 
