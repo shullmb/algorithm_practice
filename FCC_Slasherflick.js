0d0c2cb49064f254387071a52e337302275445ee
Michael Shull <dev.shullmb@gmail.com>
2015-07-27 20:57:06 -0700

    INCOMPLETE

---
+++ FCC_Slasherflick.js
@@ -0,0 +1,17 @@
+//Free Code Camp Bonfire: Slasher Flick
+
+/*
+Return the remaining elements of an array after 
+chopping off n elements from the head.
+
+.splice(start,deleteCount);
+
+ INCOMPLETE
+*/
+
+function slasher(arr, howMany) {
+  
+	return arr;
+}
+
+slasher([1,2,3],2);
