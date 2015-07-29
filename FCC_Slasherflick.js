0210b60e8cd54d31642346ed2e72ed59e82a56a1
Michael Shull <dev.shullmb@gmail.com>
2015-07-28 20:55:55 -0700

    Free Code Camp Bonfire: Slasher Flick -complete- seems pointless

--- FCC_Slasherflick.js
+++ FCC_Slasherflick.js
@@ -4,14 +4,26 @@
 Return the remaining elements of an array after 
 chopping off n elements from the head.
 
-.splice(start,deleteCount);
 
- INCOMPLETE
+inputs: array a[], integer n 
+output: array b[]; a[] less n elements such that b[0]=a[n] 
+
+if n >= a.length
+	return []
+else if n < 1
+	return a[]
+else
+	return b[]
+	(a[]less n elements such that b[0]=a[n])
+
+ Upon inspection of the docs... it appears that 
+Array.prototype.slice() does just that...
+
+so... 
 */
 
-function slasher(arr, howMany) {
-  
-	return arr;
+function slasher(arr, n) {
+	arr.slice(n);
 }
 
 slasher([1,2,3],2);
