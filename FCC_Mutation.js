0c640fedd1232058d81b83d3b5db6ae54a8e91e6
Michael Shull <dev.shullmb@gmail.com>
2015-07-29 18:04:15 -0700

    INCOMPLETE

---
+++ FCC_Mutation.js
@@ -0,0 +1,17 @@
+/*
+Free Code Camp Bonfire: Mutation
+
+Return true if the string in the first element 
+of the array contains all of the letters of 
+the string in the second element of the array.
+
+
+
+INCOMPLETE
+*/
+
+function mutation(arr) {
+	
+}
+
+mutation(['hello','hey']);
