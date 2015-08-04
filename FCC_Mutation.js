66fd5b561762d257349f6c58f9711b67e686e0c6
shullmb <dev.shullmb@gmail.com>
2015-08-03 19:02:49 -0700

    INCOMPLETE

--- FCC_Mutation.js
+++ FCC_Mutation.js
@@ -4,14 +4,27 @@ Free Code Camp Bonfire: Mutation
 Return true if the string in the first element 
 of the array contains all of the letters of 
 the string in the second element of the array.
-
-
+ 
+ Inputs: Array with two string indices
+ Ourput: Boolean True or False
+ 
+ if Array[1] contains all
 
 INCOMPLETE
 */
 
 function mutation(arr) {
+	indA = arr[0].toLowerCase();
+	indB = arr[1].toLowerCase().split('');
 	
+	for (i = 0; i < indB.length; i++) {
+		if (indA.indexOf(indB[i]) === -1 ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
+
 
 mutation(['hello','hey']);
