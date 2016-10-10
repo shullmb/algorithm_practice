8b60a6cba685267f71ffbfac58261da994b8643b
shullmb <dev.shullmb@gmail.com>
2016-10-10 15:00:31 -0700

    FCC Seek and Destroy complete

--- FCC_Destroyer.js
+++ FCC_Destroyer.js
@@ -1,19 +1,11 @@
 function destroyer(arr) {
     var args = Array.prototype.slice.call(arguments,1);
-    console.log("args: ", args );
-    
-    function byArgs(val) {
-        console.log("Array Value: ", val);
-
-        for (var i = 0; i < args.length; i++ ){
-            console.log( "Argument: ", args[i]);
-            
-             val !== args[i];
-            
-        }
-
+	
+    for ( var i = 0; i < args.length; i++ ) {
+		arr = arr.filter(function(val) {
+			return val !== args[i];
+		});
     }
-
-    newArr = arr.filter(byArgs);
-    return newArr;
+	return arr;
 }
+
